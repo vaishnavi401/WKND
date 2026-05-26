eedde836120d77c2e0b84b1d78ea4452df081750
vaishnavi401 <v.ihare21@gmail.com>
2026-05-26 20:23:25 +0530

    create project

--- ui.frontend/clientlib.config.js
+++ ui.frontend/clientlib.config.js
@@ -26,7 +26,7 @@ const CLIENTLIB_DIR = path.join(
   'content',
   'jcr_root',
   'apps',
-  'wknd',
+  'kf',
   'clientlibs'
 );
 
@@ -45,7 +45,7 @@ module.exports = {
     {
       ...libsBaseConfig,
       name: 'clientlib-dependencies',
-      categories: ['wknd.dependencies'],
+      categories: ['kf.dependencies'],
       assets: {
         // Copy entrypoint scripts and stylesheets into the respective ClientLib
         // directories
@@ -64,8 +64,8 @@ module.exports = {
     {
       ...libsBaseConfig,
       name: 'clientlib-site',
-      categories: ['wknd.site'],
-      dependencies: ['wknd.dependencies'],
+      categories: ['kf.site'],
+      dependencies: ['kf.dependencies'],
       assets: {
         // Copy entrypoint scripts and stylesheets into the respective ClientLib
         // directories
